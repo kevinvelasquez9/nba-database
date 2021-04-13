e71a2a52e528eda63bf9a36fd13add0bfd82879e
Kevin Velasquez <velasquezkevin@icloud.com>
2021-04-13 14:43:01 -0400

    Update constants.py

--- assets/constants.py
+++ assets/constants.py
@@ -1,62 +1,92 @@
 TEAM_TO_TEAM_ABBR = {
         'ATLANTA HAWKS': 'ATL',
-        'ST. LOUIS HAWKS': 'SLH',
-        'MILWAUKEE HAWKS': 'MIL',
-        'TRI-CITIES BLACKHAWKS': 'TCB',
+        'ST. LOUIS HAWKS': 'ATL',
+        'MILWAUKEE HAWKS': 'ATL',
+        'TRI-CITIES BLACKHAWKS': 'ATL',
+        
         'BOSTON CELTICS': 'BOS',
+        
         'BROOKLYN NETS': 'BRK',
-        'NEW JERSEY NETS' : 'NJN',
-        'NEW YORK NETS' : 'NYN',
+        'NEW JERSEY NETS' : 'BRK',
+        'NEW YORK NETS' : 'BRK',
+        
         'CHICAGO BULLS': 'CHI',
-        'CHARLOTTE HORNETS': 'CHO',
+        
+        'CHARLOTTE HORNETS': 'CHA',
         'CHARLOTTE BOBCATS' : 'CHA',
+        
         'CLEVELAND CAVALIERS': 'CLE',
+        
         'DALLAS MAVERICKS': 'DAL',
+        
         'DENVER NUGGETS': 'DEN',
+        
         'DETROIT PISTONS': 'DET',
-        'FORT WAYNE PISTONS': 'FWP',
+        'FORT WAYNE PISTONS': 'DET',
+        
         'GOLDEN STATE WARRIORS': 'GSW',
-        'SAN FRANCISCO WARRIORS': 'SFW',
-        'PHILADELPHIA WARRIORS': 'PHI',
+        'SAN FRANCISCO WARRIORS': 'GSW',
+        'PHILADELPHIA WARRIORS': 'GSW',
+        
         'HOUSTON ROCKETS': 'HOU',
         'SAN DIEGO ROCKETS': 'HOU',
+        
         'INDIANA PACERS': 'IND',
+        
         'LOS ANGELES CLIPPERS': 'LAC',
-        'SAN DIEGO CLIPPERS': 'SDC',
-        'BUFFALO BRAVES': 'BUF',
+        'SAN DIEGO CLIPPERS': 'LAC',
+        'BUFFALO BRAVES': 'LAC',
+        
         'LOS ANGELES LAKERS': 'LAL',
-        'MINNEAPOLIS LAKERS': 'MIN',
+        'MINNEAPOLIS LAKERS': 'LAL',
+        
         'MEMPHIS GRIZZLIES': 'MEM',
-        'VANCOUVER GRIZZLIES' : 'VAN',
+        'VANCOUVER GRIZZLIES' : 'MEM',
+        
         'MIAMI HEAT': 'MIA',
+        
         'MILWAUKEE BUCKS': 'MIL',
+        
         'MINNESOTA TIMBERWOLVES': 'MIN',
+        
         'NEW ORLEANS PELICANS' : 'NOP',
-        'NEW ORLEANS/OKLAHOMA CITY HORNETS' : 'NOK',
-        'NEW ORLEANS HORNETS' : 'NOH',
+        'NEW ORLEANS/OKLAHOMA CITY HORNETS' : 'NOP',
+        'NEW ORLEANS HORNETS' : 'NOP',
+        
         'NEW YORK KNICKS' : 'NYK',
+        
         'OKLAHOMA CITY THUNDER' : 'OKC',
-        'SEATTLE SUPERSONICS' : 'SEA',
+        'SEATTLE SUPERSONICS' : 'OKC',
+        
         'ORLANDO MAGIC' : 'ORL',
+        
         'PHILADELPHIA 76ERS' : 'PHI',
-        'SYRACUSE NATIONALS' : 'SYR',
+        'SYRACUSE NATIONALS' : 'PHI',
+        
         'PHOENIX SUNS' : 'PHO',
+        
         'PORTLAND TRAIL BLAZERS' : 'POR',
+        
         'SACRAMENTO KINGS' : 'SAC',
-        'KANSAS CITY KINGS' : 'KCK',
-        'KANSAS CITY-OMAHA KINGS' : 'KCK',
-        'CINCINNATI ROYALS' : 'CIN',
-        'ROCHESTER ROYALS': 'ROR',
+        'KANSAS CITY KINGS' : 'SAC',
+        'KANSAS CITY-OMAHA KINGS' : 'SAC',
+        'CINCINNATI ROYALS' : 'SAC',
+        'ROCHESTER ROYALS': 'SAC',
+        
         'SAN ANTONIO SPURS' : 'SAS',
+        
         'TORONTO RAPTORS' : 'TOR',
+        
         'UTAH JAZZ' : 'UTA',
-        'NEW ORLEANS JAZZ' : 'NOJ',
+        
+        'NEW ORLEANS JAZZ' : 'UTA',
+        
         'WASHINGTON WIZARDS' : 'WAS',
         'WASHINGTON BULLETS' : 'WAS',
-        'CAPITAL BULLETS' : 'CAP',
-        'BALTIMORE BULLETS' : 'BAL',
-        'CHICAGO ZEPHYRS' : 'CHI',
-        'CHICAGO PACKERS' : 'CHI',
+        'CAPITAL BULLETS' : 'WAS',
+        'BALTIMORE BULLETS' : 'WAS',
+        'CHICAGO ZEPHYRS' : 'WAS',
+        'CHICAGO PACKERS' : 'WAS',
 
         # DEFUNCT FRANCHISES
         'ANDERSON PACKERS': 'AND',
