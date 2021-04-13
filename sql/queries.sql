0a5d61b8d247b805bc0adc5ed2c11fc159d7a28a
Kevin Velasquez <velasquezkevin@icloud.com>
2021-04-13 00:05:03 -0400

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -9,6 +9,16 @@ FROM MVP NATURAL JOIN Statistics;
 SELECT DISTINCT a1.mvp
 FROM Award AS a1 JOIN Award AS a2 ON a1.mvp = a2.rookieOfTheYear;
 
+-- Question 5
+WITH T1 AS (SELECT AVG(distance) AS 'avgDistanceOf2000s'
+            FROM BuzzerBeater
+            WHERE date_recorded LIKE '%200%'),
+     T2 AS (SELECT AVG(distance) AS 'avgDistanceOf2010s'
+            FROM BuzzerBeater
+            WHERE date_recorded LIKE '%201%')
+SELECT *
+FROM T1, T2;
+
 -- Question 15
 WITH MVP AS (SELECT playerName, COUNT(playerName) AS 'numBuzzerbeaters'
              FROM (SELECT DISTINCT mvp AS 'playerName'
